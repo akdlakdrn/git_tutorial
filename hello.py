76584992c7a19ad3b1ed1147cf83c5a9cbd72df2
akdlakdrn <46807135+akdlakdrn@users.noreply.github.com>
2019-04-23 10:14:26 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
+//for command line git tutorial
+//github modification
 print("hello world")
 print("tell yout world")
-<<<<<<< HEAD
 print("tell his world")
-=======
 print("tell her world")
->>>>>>> hotfix
+
